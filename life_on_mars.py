c9fbd41c733c6672b9b4cb2bf095f4c42c4e674e
c038644 <106696284+c038644@users.noreply.github.com>
2023-11-27 11:38:33 +0000

    Update life_on_mars.py

--- life_on_mars.py
+++ life_on_mars.py
@@ -41,7 +41,7 @@ flitered_df = Min_flitered_df.loc[Min_flitered_df['Planet Radius [Earth Radius]'
 Min_flitered_df = flitered_df.loc[flitered_df['Planet Mass [Earth Mass]'] > planet_mass_options[0]]
 flitered_df = Min_flitered_df.loc[Min_flitered_df['Planet Mass [Earth Mass]'] < planet_mass_options[1]]
 
-st.sidebar.write(flitered_df.shape)
+st.sidebar.write('You have selected', flitered_df.shape[0], 'planets')
 
 Planet = st.selectbox('Select Planet', flitered_df['Planet Name'], help = 'Filter report to show only one exoplanet')
 Selected_Planet = Exoplanet_df.loc[Exoplanet_df['Planet Name'] == Planet]
