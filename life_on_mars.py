ca607f4131d27d45fc03776bd9d0a34037c2aaaf
c038644 <106696284+c038644@users.noreply.github.com>
2023-11-29 13:22:39 +0000

    Update life_on_mars.py

--- life_on_mars.py
+++ life_on_mars.py
@@ -172,7 +172,7 @@ elif Input_Selector == 'Goldilocks Calculator':
                 {'range': [34, 66], 'color': 'orange'},
                 {'range': [67, 100], 'color': 'green'}]}))
 
- g1.plotly_chart(fig1, use_container_width=True)
+ g1[0].plotly_chart(fig1, use_container_width=True)
   
  #filtered_df = filtered_df.loc[
  #       (filtered_df['Planet Radius [Earth Radius]'] > input_min_radius) &
