08cec01518118bc08b2abb5075ad6ff5c15ccc5e
c038644 <106696284+c038644@users.noreply.github.com>
2023-11-29 12:43:12 +0000

    Update life_on_mars.py

--- life_on_mars.py
+++ life_on_mars.py
@@ -158,7 +158,7 @@ elif Input_Selector == 'Goldilocks Calculator':
  
  fig1 = go.Figure(go.Indicator(
         mode = "gauge+number+delta",
-        value = Selected_Planet['Score'],#Selected_Planet.iat[0,12],
+        value = Selected_Planet.iat[0,12],
         domain = {'x': [0, 1], 'y': [0, 1]},
         title = {'text': "Life Ranking", 'font': {'size': 24}},
         gauge = {
